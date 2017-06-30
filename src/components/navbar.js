f16feb4efdfbaae4c581ac4b85013fb43ef60ef6
Victor <victoraweb@gmail.com>
2017-06-30 19:30:02 -0300

    [FEAT] Header in a separate file

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+
+import PostsIndex from '../components/post_index';
+import PostsNew from '../components/post_new';
+
 export default (props) => {
     return(
-        <div>
-            <Link to="/">Posts</Link>
-            <Link to="/posts/new">Add post</Link>
-        </div>
+        <BrowserRouter>
+            <div>
+                Header
+                <Switch>
+                    <Route path="/posts/new" component={PostsNew} />
+                    <Route path="/" component={PostsIndex} />
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
 };
--- src/index.js
+++ src/index.js
@@ -2,27 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
-import Header from './components/navbar';
 
 // import App from './components/app'; This is no longer needed since there is no more start component to the application
 import reducers from './reducers';
-import PostsIndex from './components/post_index';
-import PostsNew from './components/post_new';
+
+import Header from './components/navbar';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter>
-      <div>
-        Header
-        <Switch>
-          <Route path="/posts/new" component={PostsNew} />
-          <Route path="/" component={PostsIndex} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <Header />    
   </Provider>
   , document.querySelector('.container'));
